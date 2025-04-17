550e11873ecf7ff0dbb56c4fc05ff1fd33a9e8d0
emachizy <emachi2011@gmail.com>
2025-04-18 00:56:50 +0100

    seller/admin login logic added

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -20,7 +20,7 @@ const SellerLogin = () => {
     !isSeller && (
       <form
         onSubmit={onSubmitHandler}
-        className="max-h-screen flex items-center text-sm text-gray-600"
+        className="min-h-screen flex items-center text-sm text-gray-600"
       >
         <div className="flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-80 rounded-lg shadow-xl border border-gray-200">
           <p className="text-2xl font-medium m-auto">
@@ -32,6 +32,9 @@ const SellerLogin = () => {
               type="email"
               placeholder="Enter your email"
               className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
           <div className="w-full">
@@ -40,8 +43,14 @@ const SellerLogin = () => {
               type="password"
               placeholder="Enter your password"
               className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
             />
           </div>
+          <button className="bg-primary text-white w-full py-2 rounded-md cursor-pointer">
+            Login
+          </button>
         </div>
       </form>
     )
