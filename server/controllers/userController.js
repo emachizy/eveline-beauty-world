b6ecbe11e0bce8506963ee057cba9bb746f2874f
emachizy <emachi2011@gmail.com>
2025-04-21 13:13:19 +0100

    Logic logic and route tested and working

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -72,10 +72,17 @@ export const login = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: true, // The cookie is accessible only to the web server. Not to the client.
+      httpOnly: true,
       secure: process.env.NODE_ENV === "production", // The cookie is sent over a secure protocol (HTTPS).
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-  } catch (error) {}
+    return res.json({
+      success: true,
+      user: { email: user.email, name: user.name },
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.json({ success: false, message: error.message });
+  }
 };
