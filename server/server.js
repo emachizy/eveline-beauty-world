b84792ad6806a5f443d5e3225ef7e4ece50d4d0a
emachizy <emachi2011@gmail.com>
2025-04-28 01:04:54 +0100

    allowed origins updated

--- server/server.js
+++ server/server.js
@@ -18,7 +18,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://eveline-beauty-world.vercel.app",
+    ],
     credentials: true,
   })
 );
