5349b5e49c77d6912c09a7b2ec93e9e53eec3ab9
emachizy <emachi2011@gmail.com>
2025-05-04 02:07:33 +0100

    railway backend updated

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ const startServer = async () => {
   try {
     await connectDB();
     await connectCloudinary();
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
   } catch (error) {
